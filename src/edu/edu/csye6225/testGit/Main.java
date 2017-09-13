583f596bbc6047dae9b93c8f92002f196d9c68ca
ckeskar <keskar.c@husky.neu.edu>
2017-09-13 12:52:02 -0400

    Test Commit 3

--- src/edu/edu/csye6225/testGit/Main.java
+++ src/edu/edu/csye6225/testGit/Main.java
@@ -2,7 +2,9 @@ package edu.edu.csye6225.testGit;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        System.out.println("Hi Git");
         System.out.println("Hello Git!!");
     }
 }
